883c4c6fa012396e8c43a7e9ee1fdf0f75b8e2ec
yasuko_sekine <kuronene23@gmail.com>
2018-03-07 05:38:29 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,21 +13,27 @@
   		
   		<div class="collapse navbar-collapse" id="bs-example-navber-collapse-1">
   			<ul class="nav navbar-nav navbar-right">
-  			  <!--@if (Auth::check())-->
-    			<!--  <li>{!! link_to_route('users.index', 'Users') !!}</li>-->
-    			<!--  <li class="dropdown">-->
-    			<!--    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>-->
-       <!--       <ul class="dropdown-menu">-->
-       <!--         <li>{!! link_to_route('users.show', 'My profile', ['id' => Auth::user()->id]) !!}</li>-->
-       <!--         <li>{!! link_to_route('users.favorites', 'Favorites', ['id' => Auth::user()->id]) !!}</li>-->
-       <!--         <li role="separator" class="divider"></li>-->
-       <!--         <li>{!! link_to_route('logout.get', 'Logout') !!}</li>-->
-       <!--       </ul>-->
-    			<!--  </li>-->
-  			  <!-- @else-->
+  			  @if (Auth::check())
+  			    <li>
+  			      <a href="#"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>アイテムを追加</a>
+  			    </li>
+  			    
+    			  <li class="dropdown">
+    			    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+    			      <span class="gravatar"><img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle"></span>
+    			      {{ Auth::user()->name }} 
+    			      <span class="caret"></span>
+    			    </a>
+              <ul class="dropdown-menu">
+                <li><a href="#">マイページ</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="{{ route('logout.get') }}">ログアウト</a></li>
+              </ul>
+    			  </li>
+  			   @else
     				<li><a href="{{ route('signup.get') }}">新規登録</a></li>
-    				<li><a href="#">ログイン</a></li>
-    			 <!--@endif-->
+    				<li><a href="{{ route('login.get') }}">ログイン</a></li>
+    			 @endif
   			</ul>
   		</div>
   	</div>
