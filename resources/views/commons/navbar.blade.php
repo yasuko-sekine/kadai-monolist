d30071af68d8f3277f7220b4804c1df16818afdd
yasuko_sekine <kuronene23@gmail.com>
2018-03-07 11:15:28 +0000

    rakuten api

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -15,7 +15,7 @@
   			<ul class="nav navbar-nav navbar-right">
   			  @if (Auth::check())
   			    <li>
-  			      <a href="#"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>アイテムを追加</a>
+  			      <a href="{{ route('items.create') }}"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>アイテムを追加</a>
   			    </li>
   			    
     			  <li class="dropdown">
